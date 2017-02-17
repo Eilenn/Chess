99a5a636fe748648415d72e298be1ecdc4a16f07
Bogna Wróbel <bogna.wrobel@capgemini.com>
2017-02-17 08:28:49 +0100

    empty chess project

---
+++ test/com/capgemini/chess/BoardTest.java
@@ -0,0 +1,14 @@
+package com.capgemini.chess;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BoardTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
