5fdc1f6d73e657ca9eff1054bcc8335adbe48a28
Bogna Wróbel <bogna.wrobel@capgemini.com>
2017-02-21 22:10:32 +0100

    wouldMoveCauseCheck added in Piece.class, not tested nor implemented

--- src/com/capgemini/chess/Piece.java
+++ src/com/capgemini/chess/Piece.java
@@ -41,10 +41,35 @@ public abstract class Piece {
 
 	public boolean wouldMoveCauseCheck(Coordinate from, Coordinate to, Square[][] chessboard) {
 		// move piece to target, set empty at origin
-		Piece pieceBeingMoved=chessboard[from.getRow()][from.getColumn()].getPiece();
+		Piece pieceBeingMoved = chessboard[from.getRow()][from.getColumn()].getPiece();
+		ColorChess pieceBeingMovedColor = pieceBeingMoved.getColor();
+		ColorChess opponentColor = returnOpponentColor(pieceBeingMovedColor);
+		Piece pieceAtDestination=chessboard[to.getRow()][to.getColumn()].getPiece();
 		chessboard[to.getRow()][to.getColumn()].setPiece(pieceBeingMoved);
-		Piece empty=new EmptyPiece();
+		Piece empty = new EmptyPiece();
 		chessboard[from.getRow()][from.getColumn()].setPiece(empty);
+		// find King of this team position
+		Coordinate kingsPosition = getSameTeamKingsPosition(pieceBeingMovedColor, chessboard);
+		// iterate over board
+		for (int row = 1; row <= 8; row++) {
+			for (int col = 1; col <= 8; col++) {
+				Coordinate fromOpponent = new Coordinate(row, col);
+				Square sq = chessboard[row][col];
+				Piece piece = sq.getPiece();
+				ColorChess pieceColor = piece.getColor();
+				if (!sq.isEmpty() && (pieceColor == opponentColor)) {
+					if (piece.isMoveValid(fromOpponent, kingsPosition, chessboard)) {
+						// return piece to original position because move is impossible as it causes self check
+						chessboard[from.getRow()][from.getColumn()].setPiece(pieceBeingMoved);
+						chessboard[to.getRow()][to.getColumn()].setPiece(pieceAtDestination);
+						return true;
+					}
+
+				}
+			}
+		}
+		chessboard[from.getRow()][from.getColumn()].setPiece(pieceBeingMoved);
+		chessboard[to.getRow()][to.getColumn()].setPiece(pieceAtDestination);
 		return false;
 	}
 
@@ -53,6 +78,25 @@ public abstract class Piece {
 			return white;
 		else
 			return black;
+	}
+
+	private Coordinate getSameTeamKingsPosition(ColorChess kingsColor, Square[][] chessboard) {
+		Coordinate kingsPosition = new Coordinate(0, 0);
+		Square sq;
+		ColorChess pieceColor;
+		Piece piece;
+		for (int row = 1; row <= 8; row++) {
+			for (int col = 1; col <= 8; col++) {
+				sq = chessboard[row][col];
+				piece = sq.getPiece();
+				pieceColor = piece.getColor();
+				if (!sq.isEmpty() && (pieceColor == kingsColor) && (piece instanceof King)) {
+					kingsPosition = new Coordinate(row, col);
+				}
+			}
+		}
+		return kingsPosition;
+
 	}
 
 }
--- test/com/capgemini/chess/WhitePawnTest.java
+++ test/com/capgemini/chess/WhitePawnTest.java
@@ -201,4 +201,5 @@ public class WhitePawnTest {
 		// then
 		assertFalse(isAllowed);
 	}
+
 }
